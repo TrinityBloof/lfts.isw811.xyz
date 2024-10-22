95a2eb34b1264adb423a49c4924c7140474a65c5
TrinityBloof <naruto10890@gmail.com>
2024-10-22 09:20:14 -0600

    Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,43 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-        
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My First Post',
-            'slug' => 'my-first-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quisquam, voluptate.</p>',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quisquam, voluptate.</p>',
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
--- docs/Working with databases/turbo_boost_with_factories.md
+++ docs/Working with databases/turbo_boost_with_factories.md
@@ -4,5 +4,5 @@
 
 # Turbo Boost With Factories
 
-- En este capítulo 
-![Virtualización](/docs/images/cap23.png)
+- En este capítulo se mejoro el seeder creando factories para los posts y las categorías, haciendo que se más fácil crear datos random para tests.
+![Virtualización](/docs/images/cap28.png)
