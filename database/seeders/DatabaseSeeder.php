00cc0f5519c1749c694fe12b8e3a47c20c7beaea
TrinityBloof <naruto10890@gmail.com>
2024-10-22 08:59:41 -0600

    Database Seeding Saves Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+        
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My First Post',
+            'slug' => 'my-first-post',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quisquam, voluptate.</p>',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate. Quisquam, voluptate.</p>',
+        ]);
     }
 }
--- docs/Working with databases/database_seeding_saves_time.md
+++ docs/Working with databases/database_seeding_saves_time.md
@@ -4,5 +4,5 @@
 
 # Database Seeding Saves Time
 
-- En este capítulo 
-![Virtualización](/docs/images/cap23.png)
+- En este capítulo vimos como utilizar los seeders para crear semillas que contengan información para nuestra base de datos y evitar tener que cargar datos de forma manual cada vez que haciamos un fresh, además de incluir el creador del post dentro de cada post.
+![Virtualización](/docs/images/cap27.png)
